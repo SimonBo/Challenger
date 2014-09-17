4c52352bb0d824ff5a5b5a9f2cd6c4e607d67495
Szymon Borucki <szymon.borucki@gmail.com>
2014-09-17 14:37:04 -0400

    Created and seeded seed file with users and challenges

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |i|
+  User.create(username: "user#{i}", email: "user#{i}@email.com", password: 'useruser', password_confirmation: 'useruser')
+end
+
+challenge_name = ['Ice Bucket', 'Eat a dog', 'Steal somebody\'s wallet', 'Tell your boss you want to rape his wife/husband', 'Try to mainain erection for 10 hours']
+description = "Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Sed posuere consectetur est at lobortis."
+
+5.times do |i|
+  Challenge.create(name: challenge_name[i], description: description)
+end
