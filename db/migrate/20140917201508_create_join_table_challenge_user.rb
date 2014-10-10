0ee69023b7ac5621d15cae03e87839586088427b
Szymon Borucki <szymon.borucki@gmail.com>
2014-10-10 13:30:00 -0400

    Deleted two migrations that were causing errors

--- db/migrate/20140917201508_create_join_table_challenge_user.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableChallengeUser < ActiveRecord::Migration
-  def change
-    create_join_table :challenges, :users do |t|
-      # t.index [:challenge_id, :user_id]
-      # t.index [:user_id, :challenge_id]
-    end
-  end
-end
