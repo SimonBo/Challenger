993e00bbfaffe661399d9d92608bdc546e1bf5bb
Szymon Borucki <szymon.borucki@gmail.com>
2014-10-21 08:06:38 -0400

    Deleted migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141016214436) do
+ActiveRecord::Schema.define(version: 20141021012603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,7 +76,7 @@ ActiveRecord::Schema.define(version: 20141016214436) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "stripe_customer_token"
-    t.string   "provider"
+    t.string   "provider",               default: ""
     t.string   "uid"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
