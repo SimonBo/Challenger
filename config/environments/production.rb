79b621b9113bd4bee6273d0277d1f5f1839f466f
Szymon Borucki <szymon.borucki@gmail.com>
2014-10-27 17:16:45 -0400

    Added mandril mail for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+  :address   => "smtp.mandrillapp.com",
+  :port      => 587,
+  :enable_starttls_auto => true,
+  :user_name => ENV["MANDRILL_USERNAME"],
+  :password  => ENV["MANDRILL_API_KEY"],
+  :authentication => 'login',
+  :domain => "smtp.mandrillapp.com"
+  }
 end
